f2a998d1c570ab0c7bba67feb0ec9ee2642c3a0a
Luiz Fernando Franzon <luizfranzon05@hotmail.com>
2024-05-16 20:48:42 -0300

    add: auto redirect when loading homepage (temporary)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
+"use client"
+
 import Link from "next/link";
+import { useEffect } from "react";
 
 export default function Home() {
+
+  useEffect(() => {
+    window.location.href = "/app";
+  }, [])
+
   return (
     <div>
       <Link href="/app">Home</Link>
