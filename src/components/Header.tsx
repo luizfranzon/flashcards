52a50eaf0b9a2e733c6487fb535c44cfa98563ca
Luiz Fernando Franzon <luizfranzon05@hotmail.com>
2024-09-24 17:49:04 -0300

    fix: remove redirect when click on logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,9 +11,7 @@ export function Header() {
   return (
     <header className="border-b p-5">
       <nav className="m-auto max-w-7xl flex items-center justify-between">
-        <Link href={"/app"}>
           <h1 className="font-black text-2xl">FlashCards.</h1>
-        </Link>
         <CreateCardModal setCardData={setCardData}>
           <Button className="font-bold">Criar Flashcard</Button>
         </CreateCardModal>
